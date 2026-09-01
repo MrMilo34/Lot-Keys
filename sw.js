61d79722e78d8c9562a77def200f846c49aabf8b
Milo34 <BlairK34@Live.com>
2026-09-01 15:06:01 -0600

    Add files via upload
    New Logo for the app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='lotkeys-drive-test-v0913';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./assets/carfax-one-owner.png','./assets/carfax-low-kilometres.png','./assets/carfax-no-reported-accidents.png'];
+const CACHE='lotkeys-drive-test-v0914';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./assets/carfax-one-owner.png','./assets/carfax-low-kilometres.png','./assets/carfax-no-reported-accidents.png', './assets/lotkeys-default-logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
